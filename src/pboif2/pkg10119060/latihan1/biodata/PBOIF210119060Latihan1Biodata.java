ab4d9fb257ffa5630f6d0a939972cfac5de7fa78
asus <asus@LAPTOP-BD2P4KVT>
2020-10-15 19:09:33 +0700

    new commit

---
+++ src/pboif2/pkg10119060/latihan1/biodata/PBOIF210119060Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119060.latihan1.biodata;
+
+/**
+ *
+ * @author asus
+ * Nama         : Megianto Adi Saputra
+ * Kelas        : IF-2
+ * NIM          : 10119060
+ * Deskripsi    : Program ini berisi program menampilkan biodata
+ */
+public class PBOIF210119060Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "-------------------");
+        System.out.println("NAMA\t\t: Megianto Adi Saputra");
+        System.out.println("ALAMAT\t\t: Jalan Skeloa");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 19 mei 2001");
+        System.out.println("------------------------------------------------------");
+        System.out.println("HOBI\t\t: berenang");
+        System.out.println("MAKES\t\t: bakso");
+        System.out.println("MIKES\t\t: es campur");
+    }
+    
+}
